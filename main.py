db66a75836f5556cb16f1b0fcc74c9b0d35cfb26
NNAMDI ADIGWE <145211869+nnam-dev@users.noreply.github.com>
2024-10-03 16:14:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -404,4 +404,4 @@ def download_best():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Adjust port as necessary
+    app.run(debug=True,host='0.0.0.0', port=5000)  # Adjust port as necessary
